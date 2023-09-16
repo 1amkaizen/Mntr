b285cf27128be807c1e81418737cf33b9ad97863
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-16 22:05:15 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
-package main
-
 import (
 	"html/template"
 	"net/http"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log" 
+
 	"github.com/1amkaizen/telegoGPT/models"
 )
 
@@ -14,20 +12,16 @@ func displayData(w http.ResponseWriter, r *http.Request) {
 // Konfigurasi koneksi ke database
 	dsn := "root:5YUTSCHvx0yXwcsJUQFW@tcp(containers-us-west-139.railway.app:6522)/railway" 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-if err != nil {
-    log.Fatal("Failed to connect to database:", err)
-}
-defer db.Close()
-
-log.Println("Connected to database!")
-
-
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	// Mengambil data dari database
 		// Mengambil data dari database
 	var messages []models.Messages
 	db.Find(&messages)
-log.Println("Messages from database:", messages)
+
 	// Membaca konten dari file index.html
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
@@ -46,5 +40,4 @@ log.Println("Messages from database:", messages)
 func main() {
 	http.HandleFunc("/", displayData)
 	http.ListenAndServe(":6522", nil)
-	
 }
